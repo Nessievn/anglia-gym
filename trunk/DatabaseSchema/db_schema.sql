199ef7b30f6e5874fc2d1d43149317be5231982b
skarvin@gmail.com <skarvin@gmail.com@f61b0cec-0cda-11df-a5af-b38b39d06e06>
2010-02-27 23:41:48 +0000

    Mysql Dump Script updated

--- trunk/DatabaseSchema/db_schema.sql
+++ trunk/DatabaseSchema/db_schema.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.1.40, for apple-darwin9.5.0 (i386)
+CREATE DATABASE  IF NOT EXISTS `gym` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `gym`;
+-- MySQL dump 10.13  Distrib 5.1.40, for Win32 (ia32)
 --
--- Host: localhost    Database: gym_db
+-- Host: localhost    Database: gym
 -- ------------------------------------------------------
--- Server version	5.1.40
+-- Server version	5.1.44-community
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +18,74 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `members`
+--
+
+DROP TABLE IF EXISTS `members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `members` (
+  `id_member` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(60) NOT NULL,
+  `lastName` varchar(60) NOT NULL,
+  `birthdate` date NOT NULL,
+  `address_1` text,
+  `city` varchar(60) DEFAULT NULL,
+  `county` varchar(60) DEFAULT NULL,
+  `postalcode` varchar(15) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  `payment_method` varchar(50) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT '1',
+  `address_2` text,
+  `emerg_contact_name` varchar(45) DEFAULT NULL,
+  `emerg_contact_relation` varchar(45) DEFAULT NULL,
+  `emerg_contact_phone` varchar(45) DEFAULT NULL,
+  `emerg_contact_mobile` varchar(45) DEFAULT NULL,
+  `allergies` text,
+  `medical_notes` text,
+  `picture` varchar(150) NOT NULL,
+  PRIMARY KEY (`id_member`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `members_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `members`
+--
+
+LOCK TABLES `members` WRITE;
+/*!40000 ALTER TABLE `members` DISABLE KEYS */;
+/*!40000 ALTER TABLE `members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rooms`
+--
+
+DROP TABLE IF EXISTS `rooms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rooms` (
+  `id_room` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `description` text,
+  `size` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_room`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rooms`
+--
+
+LOCK TABLES `rooms` WRITE;
+/*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `class_bookings`
 --
 
@@ -27,51 +97,50 @@ CREATE TABLE `class_bookings` (
   `id_member` int(11) DEFAULT NULL,
   `id_class_instance` int(11) DEFAULT NULL,
   `booking_date` date DEFAULT NULL,
-  PRIMARY KEY (`id_class_booking`)
+  PRIMARY KEY (`id_class_booking`),
+  KEY `FK_class_bookings_1` (`id_class_instance`),
+  CONSTRAINT `FK_class_bookings_1` FOREIGN KEY (`id_class_instance`) REFERENCES `class_instance` (`id_class_instance`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `class_instance`
+-- Dumping data for table `class_bookings`
 --
 
-DROP TABLE IF EXISTS `class_instance`;
+LOCK TABLES `class_bookings` WRITE;
+/*!40000 ALTER TABLE `class_bookings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `class_bookings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `members_equipment_bookings`
+--
+
+DROP TABLE IF EXISTS `members_equipment_bookings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `class_instance` (
-  `id_class_instance` int(11) NOT NULL AUTO_INCREMENT,
-  `id_class` int(11) DEFAULT NULL,
-  `id_staff` int(11) DEFAULT NULL,
-  `date` date DEFAULT NULL,
-  `start_time` time DEFAULT NULL,
-  `end_time` time DEFAULT NULL,
-  `frequency` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`id_class_instance`),
-  KEY `id_class` (`id_class`),
-  KEY `id_staff` (`id_staff`),
-  CONSTRAINT `class_instance_ibfk_2` FOREIGN KEY (`id_staff`) REFERENCES `staff` (`id_staff`),
-  CONSTRAINT `class_instance_ibfk_1` FOREIGN KEY (`id_class`) REFERENCES `classes` (`id_class`)
+CREATE TABLE `members_equipment_bookings` (
+  `id_mbr_eq_booking` int(11) NOT NULL AUTO_INCREMENT,
+  `id_member` int(11) DEFAULT NULL,
+  `date_start` date DEFAULT NULL,
+  `date_due` date DEFAULT NULL,
+  `id_equipment` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_mbr_eq_booking`),
+  KEY `id_equipment` (`id_equipment`),
+  KEY `id_member` (`id_member`),
+  CONSTRAINT `id_member` FOREIGN KEY (`id_member`) REFERENCES `members` (`id_member`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `id_equipment` FOREIGN KEY (`id_equipment`) REFERENCES `equipment` (`id_equipment`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `classes`
+-- Dumping data for table `members_equipment_bookings`
 --
 
-DROP TABLE IF EXISTS `classes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `classes` (
-  `id_class` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `description` text,
-  `type` varchar(50) DEFAULT NULL,
-  `id_room` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_class`),
-  KEY `id_room` (`id_room`),
-  CONSTRAINT `classes_ibfk_1` FOREIGN KEY (`id_room`) REFERENCES `rooms` (`id_room`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `members_equipment_bookings` WRITE;
+/*!40000 ALTER TABLE `members_equipment_bookings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `members_equipment_bookings` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `equipment`
@@ -90,46 +159,38 @@ CREATE TABLE `equipment` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `equipment_bookings`
+-- Dumping data for table `equipment`
 --
 
-DROP TABLE IF EXISTS `equipment_bookings`;
+LOCK TABLES `equipment` WRITE;
+/*!40000 ALTER TABLE `equipment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `classes`
+--
+
+DROP TABLE IF EXISTS `classes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `equipment_bookings` (
-  `id_eq_bookings` int(11) NOT NULL AUTO_INCREMENT,
-  `id_member` int(11) DEFAULT NULL,
-  `date_start` date DEFAULT NULL,
-  `date_due` date DEFAULT NULL,
-  PRIMARY KEY (`id_eq_bookings`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `members`
---
-
-DROP TABLE IF EXISTS `members`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `members` (
-  `id_member` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(60) NOT NULL,
-  `surname` varchar(60) NOT NULL,
-  `birthdate` date NOT NULL,
-  `address` text,
-  `city` varchar(60) DEFAULT NULL,
-  `county` varchar(60) NOT NULL,
-  `postalcode` varchar(15) DEFAULT NULL,
+CREATE TABLE `classes` (
+  `id_class` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `description` text,
   `type` varchar(50) DEFAULT NULL,
-  `payment_method` varchar(50) DEFAULT NULL,
-  `id_user` int(11) DEFAULT NULL,
-  `is_active` tinyint(1) DEFAULT '1',
-  PRIMARY KEY (`id_member`),
-  KEY `id_user` (`id_user`),
-  CONSTRAINT `members_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
+  PRIMARY KEY (`id_class`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `classes`
+--
+
+LOCK TABLES `classes` WRITE;
+/*!40000 ALTER TABLE `classes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `classes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `payments`
@@ -151,20 +212,81 @@ CREATE TABLE `payments` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `rooms`
+-- Dumping data for table `payments`
 --
 
-DROP TABLE IF EXISTS `rooms`;
+LOCK TABLES `payments` WRITE;
+/*!40000 ALTER TABLE `payments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff_equipment_bookings`
+--
+
+DROP TABLE IF EXISTS `staff_equipment_bookings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `rooms` (
-  `id_room` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `description` text,
-  `size` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_room`)
+CREATE TABLE `staff_equipment_bookings` (
+  `id_staff_eq_booking` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id_staff` int(11) DEFAULT NULL,
+  `date_start` date DEFAULT NULL,
+  `date_due` date DEFAULT NULL,
+  `id_equipment` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_staff_eq_booking`),
+  KEY `id_staff` (`id_staff`),
+  KEY `fk_id_equipment` (`id_equipment`),
+  CONSTRAINT `fk_id_equipment` FOREIGN KEY (`id_equipment`) REFERENCES `equipment` (`id_equipment`),
+  CONSTRAINT `id_staff` FOREIGN KEY (`id_staff`) REFERENCES `staff` (`id_staff`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff_equipment_bookings`
+--
+
+LOCK TABLES `staff_equipment_bookings` WRITE;
+/*!40000 ALTER TABLE `staff_equipment_bookings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff_equipment_bookings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `class_instance`
+--
+
+DROP TABLE IF EXISTS `class_instance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `class_instance` (
+  `id_class_instance` int(11) NOT NULL AUTO_INCREMENT,
+  `id_class` int(11) DEFAULT NULL,
+  `id_staff` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `start_time` time DEFAULT NULL,
+  `end_time` time DEFAULT NULL,
+  `frequency` varchar(40) DEFAULT NULL,
+  `id_room` int(11) DEFAULT NULL,
+  `id_staff_eq_booking` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id_class_instance`),
+  KEY `id_class` (`id_class`),
+  KEY `id_staff` (`id_staff`),
+  KEY `class_instance_ibfk_3` (`id_room`),
+  KEY `FK_class_instance_4` (`id_staff_eq_booking`),
+  CONSTRAINT `FK_class_instance_4` FOREIGN KEY (`id_staff_eq_booking`) REFERENCES `staff_equipment_bookings` (`id_staff_eq_booking`) ON DELETE SET NULL ON UPDATE SET NULL,
+  CONSTRAINT `class_instance_ibfk_1` FOREIGN KEY (`id_class`) REFERENCES `classes` (`id_class`),
+  CONSTRAINT `class_instance_ibfk_2` FOREIGN KEY (`id_staff`) REFERENCES `staff` (`id_staff`),
+  CONSTRAINT `class_instance_ibfk_3` FOREIGN KEY (`id_room`) REFERENCES `rooms` (`id_room`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `class_instance`
+--
+
+LOCK TABLES `class_instance` WRITE;
+/*!40000 ALTER TABLE `class_instance` DISABLE KEYS */;
+/*!40000 ALTER TABLE `class_instance` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `staff`
@@ -175,24 +297,41 @@ DROP TABLE IF EXISTS `staff`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `staff` (
   `id_staff` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(60) NOT NULL,
-  `surname` varchar(60) NOT NULL,
+  `firstName` varchar(60) NOT NULL,
+  `lastName` varchar(60) NOT NULL,
   `birthdate` date NOT NULL,
-  `address` text,
+  `address_1` text NOT NULL,
   `city` varchar(60) DEFAULT NULL,
-  `county` varchar(60) NOT NULL,
+  `county` varchar(60) DEFAULT NULL,
   `postalcode` varchar(15) DEFAULT NULL,
   `contract_type` varchar(50) DEFAULT NULL,
   `natinsnumber` varchar(50) DEFAULT NULL,
-  `position` varchar(50) NOT NULL,
+  `position` varchar(50) DEFAULT NULL,
   `contract_start` date DEFAULT NULL,
   `contract_finish` date DEFAULT NULL,
   `id_user` int(11) DEFAULT NULL,
+  `address_2` varchar(45) DEFAULT NULL,
+  `emerg_contact_name` varchar(45) DEFAULT NULL,
+  `emerg_contact_telephone` varchar(45) DEFAULT NULL,
+  `emerg_contact_relation` varchar(45) DEFAULT NULL,
+  `nationality` varchar(80) DEFAULT NULL,
+  `allergies` varchar(90) DEFAULT NULL,
+  `medicalNotes` text,
+  `qualifications` text,
   PRIMARY KEY (`id_staff`),
   KEY `id_user` (`id_user`),
   CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff`
+--
+
+LOCK TABLES `staff` WRITE;
+/*!40000 ALTER TABLE `staff` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -210,6 +349,15 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -220,4 +368,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-02-21 21:33:36
+-- Dump completed on 2010-02-27 23:41:12
