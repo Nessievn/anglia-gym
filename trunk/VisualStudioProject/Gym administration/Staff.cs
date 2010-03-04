b14721f6c1efaff1fdc0c8fb9dce0986f037a5a9
skarvin@gmail.com <skarvin@gmail.com@f61b0cec-0cda-11df-a5af-b38b39d06e06>
2010-03-04 18:56:59 +0000

    First classes created based on the class diagram version 2.

---
+++ trunk/VisualStudioProject/Gym administration/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gym_administration
+{
+    class Staff : Person
+    {
+        public bool bSave()
+        {
+            mySqlConn conn = new mySqlConn();
+            conn.connect();
+            return true;
+        }
+    }
+}
---
+++ trunk/VisualStudioProject/Gym administration/User.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gym_administration
+{
+    class User
+    {
+        private int iId_user;
+
+        public int IId_user
+        {
+            get { return iId_user; }
+            set { iId_user = value; }
+        }
+
+        private string sLogin;
+
+        public string SLogin
+        {
+            get { return sLogin; }
+            set { sLogin = value; }
+        }
+
+        private string sPassword;
+
+        public string SPassword
+        {
+            get { return sPassword; }
+            set { sPassword = value; }
+        }
+        private string sProfile;
+
+        public string SProfile
+        {
+            get { return sProfile; }
+            set { sProfile = value; }
+        }
+
+        private bool bActive;
+
+        public bool BActive
+        {
+            get { return bActive; }
+            set { bActive = value; }
+        }
+
+        public bool bSave()
+        {
+            mySqlConn conn = new mySqlConn();
+            conn.connect();
+            string sQuery = "insert into users (id_user, login, password, profile, active) "+
+                            "values (NULL, '" + this.SLogin + "', MD5('" + this.SPassword +
+                            "'), 'member', '" + ((this.BActive) ? "1" : "0") + "')";
+           
+            this.iId_user = conn.iInsert(sQuery);
+
+            if (this.iId_user > 0)
+                return true;
+
+            return false;
+        }
+        public bool bDelete()
+        {
+            mySqlConn conn = new mySqlConn();
+            conn.connect();
+            string sQuery = "DELETE FROM users WHERE id_user = '" + this.IId_user + "'";
+            int iRes = conn.iDeleteOrUpdate(sQuery);
+            if (iRes > 0)
+                return true;
+            return false;
+        }
+    }
+}
