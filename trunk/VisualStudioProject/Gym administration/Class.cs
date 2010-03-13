92bc3b7159dcf9f7ded920c25938539e799aefff
skarvin@gmail.com <skarvin@gmail.com@f61b0cec-0cda-11df-a5af-b38b39d06e06>
2010-03-13 00:12:19 +0000

    Class administration working.

--- trunk/VisualStudioProject/Gym administration/Class.cs
+++ trunk/VisualStudioProject/Gym administration/Class.cs
@@ -7,14 +7,14 @@ using System.Windows.Forms;
 
 namespace Gym_administration
 {
-    class Room
+    class Class
     {
-        private int id_room;
+        private int id_class;
 
-        public int Id_room
+        public int Id_class
         {
-            get { return id_room; }
-            set { id_room = value; }
+            get { return id_class; }
+            set { id_class = value; }
         }
         private string sName;
 
@@ -23,12 +23,12 @@ namespace Gym_administration
             get { return sName; }
             set { sName = value; }
         }
-        private int iSize;
+        private string sType;
 
-        public int ISize
+        public string SType
         {
-            get { return iSize; }
-            set { iSize = value; }
+            get { return sType; }
+            set { sType = value; }
         }
         private string sDescription;
 
@@ -37,23 +37,23 @@ namespace Gym_administration
             get { return sDescription; }
             set { sDescription = value; }
         }
-        public Room()
+        public Class()
         {
-            this.id_room = 0;
+            this.id_class = -1;
         }
 
-        public Room(int iIdRoom)
+        public Class(int iIdClass)
         {
             mySqlConn conn = new mySqlConn();
             conn.connect();
             // We launch the query
-            List<Hashtable> lhResultset = conn.lhSqlQuery("Select * from rooms WHERE id_room = '" + iIdRoom + "'");
+            List<Hashtable> lhResultset = conn.lhSqlQuery("Select * from classes WHERE id_class = '" + iIdClass + "'");
 
             // Check if we found the member
             if ((int)lhResultset.Count > 0)
             {
-                this.Id_room = int.Parse(lhResultset[0]["id_room"].ToString());
-                this.ISize = int.Parse(lhResultset[0]["size"].ToString());
+                this.Id_class = int.Parse(lhResultset[0]["id_class"].ToString());
+                this.SType = lhResultset[0]["type"].ToString();
                 this.SDescription = lhResultset[0]["description"].ToString();
                 this.SName = lhResultset[0]["name"].ToString();
             }
@@ -61,20 +61,20 @@ namespace Gym_administration
 
         public bool bRemove()
         {
-            if (this.Id_room != 0)
+            if (this.Id_class != -1)
             {
                 mySqlConn conn = new mySqlConn();
                 conn.connect();
-                string sQuery = "DELETE FROM rooms WHERE id_room = '"+this.Id_room+"'";
+                string sQuery = "DELETE FROM classes WHERE id_class = '" + this.Id_class + "'";
                 int iRes = conn.iDeleteOrUpdate(sQuery);
                 if (iRes > 0)
                 {
-                    MessageBox.Show("The room data has been deleted succesfully!");
+                    MessageBox.Show("The class data has been deleted succesfully!");
                     return true;
                 }
                 else
                 {
-                    MessageBox.Show("There was a problem deleting the room!");
+                    MessageBox.Show("There was a problem deleting the class!");
                     return false;
                 }
             }
@@ -97,38 +97,38 @@ namespace Gym_administration
             {
                 mySqlConn conn = new mySqlConn();
                 conn.connect();
-                if (this.Id_room == 0)
+                if (this.Id_class == -1)
                 {
-                    sQuery = "insert into `gym`.`rooms` (`id_room`, `name`, `size`, `description`) values " +
-                             "(NULL, '" + this.SName + "', '" + this.ISize + "', '" + this.SDescription + "')";
+                    sQuery = "insert into `gym`.`classes` (`id_class`, `name`, `type`, `description`) values " +
+                             "(NULL, '" + this.SName + "', '" + this.SType + "', '" + this.SDescription + "')";
 
-                    int iIdRoom = conn.iInsert(sQuery);
-                    if (iIdRoom > 1)
+                    int iIdClass = conn.iInsert(sQuery);
+                    if (iIdClass != -1)
                     {
-                        this.Id_room = iIdRoom;
-                        MessageBox.Show("The new room has been added to the databse succesfully!");
+                        this.Id_class = iIdClass;
+                        MessageBox.Show("The new class has been added to the databse succesfully!");
                         return true;
                     }
                     else
                     {
-                        MessageBox.Show("There was a problem adding the new room, please check your data!");
+                        MessageBox.Show("There was a problem adding the new class, please check your data!");
                         return false;
                     }
                 }
                 else
                 {
-                    sQuery = "UPDATE rooms SET name = '" + this.SName + "', size = '" + this.ISize + "', description = '" + this.SDescription + "' " +
-                             " WHERE id_room = '" + this.Id_room + "'";
+                    sQuery = "UPDATE classes SET name = '" + this.SName + "', size = '" + this.SType + "', description = '" + this.SDescription + "' " +
+                             " WHERE id_class = '" + this.Id_class + "'";
 
                     int iRes = conn.iDeleteOrUpdate(sQuery);
                     if (iRes > 0)
                     {
-                        MessageBox.Show("The room data has been updated succesfully!");
+                        MessageBox.Show("The class data has been updated succesfully!");
                         return true;
                     }
                     else
                     {
-                        MessageBox.Show("There was a problem updating the room information, please check your data!");
+                        MessageBox.Show("There was a problem updating the class information, please check your data!");
                         return false;
                     }
                 }
---
+++ trunk/VisualStudioProject/Gym administration/frm_class.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gym_administration
+{
+    public partial class frm_class : Form
+    {
+        Class clClass;
+        public frm_class()
+        {
+            InitializeComponent();
+            clClass = new Class();
+        }
+        public frm_class(int iClassId)
+        {
+            InitializeComponent();
+            clClass = new Class(iClassId);
+            if (clClass.Id_class < 1)
+                MessageBox.Show("The class could not be found");
+            else
+            {
+                txt_classdesc.Text = clClass.SDescription;
+                txt_classname.Text = clClass.SName;
+                if (clClass.SType == "Personal")
+                    rd_personal.Checked = true;
+                else
+                    rd_group.Checked = true;
+                    
+            }
+        }
+
+        private void frm_class_Load(object sender, EventArgs e)
+        {
+            rd_group.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sType = (rd_group.Checked)?"Group":"Personal";
+
+            if(txt_classname.Text.Length == 0)
+            {
+                MessageBox.Show("Please insert a name for the class.");
+                return;
+            }
+
+            clClass.SDescription = txt_classdesc.Text;
+            clClass.SName = txt_classname.Text;
+            clClass.SType = sType;
+
+            clClass.bSave();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Are you sure?", "Delete entry", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res == DialogResult.Yes)
+            {
+                clClass.bRemove();
+                this.Close();
+            }
+        }
+
+    }
+}
