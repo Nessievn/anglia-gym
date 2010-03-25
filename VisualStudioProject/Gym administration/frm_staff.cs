39c6abe945024cf886be14597fac43ddaf2d72ec
skarvin <skarvin@gmail.com>
2010-03-25 19:42:35 +0000

    Staff Management working (thanks Katie!!)

---
+++ VisualStudioProject/Gym administration/frm_staff.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gym_administration
+{
+    public partial class frm_staff : Form
+    {
+        Staff stfStaff;
+ 
+        public frm_staff()
+        {
+            stfStaff = new Staff();
+            InitializeComponent();
+        }
+
+        public frm_staff(int iStaffId)
+        {
+            InitializeComponent();
+            
+            stfStaff = new Staff(iStaffId);
+            if (stfStaff.IId_staff < 1)
+                MessageBox.Show("The staff member could not be found");
+            else
+            {
+                 txt_firstName.Text = stfStaff.SFirstName;
+                 txt_lastName.Text = stfStaff.SLastName;
+                 txt_dob.Text = Utils.sGetCsharpDateFromMysqlDate(stfStaff.SBirthdate);
+                 txt_address1.Text = stfStaff.SAddress_1;
+                 txt_address2.Text = stfStaff.SAaddress_2;
+                 txt_city.Text = stfStaff.SCity;
+                 txt_county.Text = stfStaff.SCounty;
+                 txt_emerg_mobile.Text = stfStaff.SEmerg_contact_mobile;
+                 txt_emerg_name.Text = stfStaff.SEmerg_contact_name;
+                 txt_emerg_telephone.Text = stfStaff.SEmerg_contact_phone;
+                 txt_emerg_relation.Text = stfStaff.SEmerg_contact_relation;
+                 txt_allergies.Text = stfStaff.SMedical_allergies;
+                 txt_doctor_name.Text = stfStaff.SMedical_doctor_name;
+                 txt_medical_notes.Text = stfStaff.SMedical_notes;
+                 txt_qualifications.Text = stfStaff.SQualifications;
+                 txt_doctor_phone.Text = stfStaff.SMedical_phone;
+                 txt_pc.Text = stfStaff.SPostalcode;
+                 cmb_contract.Text = stfStaff.SContractType;
+                 txt_email.Text = stfStaff.SEmail;
+                 txt_mobile.Text = stfStaff.SMobile;
+                 txt_telephone.Text = stfStaff.SPhone;
+                 cmb_position.Text = stfStaff.SPosition;
+                 txt_natinsnumb.Text = stfStaff.SNatInsNumb;
+                 txt_contract_start.Text = stfStaff.SContractStart;
+                 txt_contract_finish.Text = stfStaff.SContractFinish;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frm_staff_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmb_securitylevel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            stfStaff.SFirstName = txt_firstName.Text;
+            stfStaff.SLastName = txt_lastName.Text;
+            stfStaff.SBirthdate = txt_dob.Text;
+            stfStaff.SAaddress_2 = txt_address2.Text;
+            stfStaff.SAddress_1 = txt_address1.Text;
+            stfStaff.SCity = txt_city.Text;
+            stfStaff.SCounty = txt_county.Text;
+            stfStaff.SEmerg_contact_mobile = txt_emerg_mobile.Text;
+            stfStaff.SEmerg_contact_name = txt_emerg_name.Text;
+            stfStaff.SEmerg_contact_phone = txt_emerg_telephone.Text;
+            stfStaff.SEmerg_contact_relation = txt_emerg_relation.Text;
+            stfStaff.SMedical_allergies = txt_allergies.Text;
+            stfStaff.SMedical_doctor_name = txt_doctor_name.Text;
+            stfStaff.SMedical_notes = txt_medical_notes.Text;
+            stfStaff.SQualifications = txt_qualifications.Text;
+            stfStaff.SMedical_phone = txt_doctor_phone.Text;
+            stfStaff.SPosition = cmb_position.Text;
+            stfStaff.SPicture = "none";
+            stfStaff.SPostalcode = txt_pc.Text;
+            stfStaff.SContractType = cmb_contract.Text;
+            stfStaff.SEmail = txt_email.Text;
+            stfStaff.SPhone = txt_telephone.Text;
+            stfStaff.SMobile = txt_mobile.Text;
+            stfStaff.SNatInsNumb = txt_natinsnumb.Text;
+            stfStaff.SContractStart = txt_contract_start.Text;
+            stfStaff.SContractFinish = txt_contract_finish.Text;
+            
+
+            stfStaff.bSave();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ VisualStudioProject/Gym administration/frm_staff_list.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Gym_administration
+{
+    partial class frm_staff_list
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_dob = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_lastName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_firstName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dg_staff = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dg_staff)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_dob
+            // 
+            this.txt_dob.Location = new System.Drawing.Point(11, 65);
+            this.txt_dob.Mask = "00/00/0000";
+            this.txt_dob.Name = "txt_dob";
+            this.txt_dob.Size = new System.Drawing.Size(177, 20);
+            this.txt_dob.TabIndex = 24;
+            this.txt_dob.ValidatingType = typeof(System.DateTime);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(194, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "E-Mail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Date of Birth";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(197, 65);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(177, 20);
+            this.txt_email.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(194, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Last Name";
+            // 
+            // txt_lastName
+            // 
+            this.txt_lastName.Location = new System.Drawing.Point(197, 26);
+            this.txt_lastName.Name = "txt_lastName";
+            this.txt_lastName.Size = new System.Drawing.Size(177, 20);
+            this.txt_lastName.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "First Name";
+            // 
+            // txt_firstName
+            // 
+            this.txt_firstName.Location = new System.Drawing.Point(11, 26);
+            this.txt_firstName.Name = "txt_firstName";
+            this.txt_firstName.Size = new System.Drawing.Size(177, 20);
+            this.txt_firstName.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(389, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dg_staff
+            // 
+            this.dg_staff.AllowUserToAddRows = false;
+            this.dg_staff.AllowUserToDeleteRows = false;
+            this.dg_staff.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg_staff.Location = new System.Drawing.Point(10, 97);
+            this.dg_staff.Name = "dg_staff";
+            this.dg_staff.Size = new System.Drawing.Size(641, 305);
+            this.dg_staff.TabIndex = 13;
+            this.dg_staff.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dg_staff_CellDoubleClick);
+            this.dg_staff.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dg_staff_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(535, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 23);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Add Staff Member";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(532, 410);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 23);
+            this.button3.TabIndex = 26;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frm_staff_list
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 445);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txt_dob);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_lastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_firstName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dg_staff);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frm_staff_list";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Staff List";
+            this.Load += new System.EventHandler(this.frm_staff_list_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg_staff)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox txt_dob;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_lastName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_firstName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dg_staff;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
