ef8db13166fd45d407b7f808a19fa819b9a3d5d3
sam.pratley <sam.pratley@f61b0cec-0cda-11df-a5af-b38b39d06e06>
2010-04-03 07:48:37 +0000

    replaces the wronglogin.php - contains login script and section to prevent sql injections

---
+++ trunk/Website/checklogin.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>:::: Checking Login ::::</title>
+<link rel="stylesheet" id="styleSheet" type="text/css" href="Styles/nonmembertemplate.css" media="screen"/>
+</head>
+
+<body>
+<form>
+<div id="wrapper">
+  <div id="masthead"><a href="home.php"><img border="0" src="images/bannernew.png" height="100%" width="100%" alt="Trees banner image"/></a></div>
+    <div id="content"></div>
+    <div id="leftpanel">
+  	 	<div id="login">Please enter your membership details and click the button below to log in.
+			<table>
+	            <tr></tr>
+            	<tr></tr>
+                	<td>Membership Number: </td>
+                    <td><input type="text" /></td>
+                <tr></tr>
+                	<td>Password: </td>
+                    <td><input type="text" /></td>
+                <tr></tr>
+                	<td></td>
+                	<td><input type="submit" value="Login"/></td>
+            </table>
+        </div>
+    	<div id="menu">
+	    	<a href="register.php"><img border="0" src="images/nomembermenu1_1.png" alt="Become a Member"/></a>
+            <a href="staff.php"><img border="0" src="images/nomembermenu2_2.png" alt="Meet the Staff"/></a>
+            <a href="contact.php"><img border="0" src="images/nomembermenu3_3.png" alt="Contact Us"/></a>
+      </div>
+    </div>
+    <div id="centre">
+	<?php
+	
+	//based on script from http://www.phpeasystep.com/phptu/6.html - has a snippet to prevent SQL injections
+	
+	$host="localhost";
+	$username="gym";
+	$password="gym";
+	$dbname="gym";
+	$tbl="members";
+	
+	mysql_connect("$host","$username","$password") or die("cannot connect");
+	mysql_select_db("$dbname","$tbl") or die ("cannot select database");
+	
+	//this protects against MySQL query injections
+	$email = stripslashes($email);
+	$memberno = stripslashes($memberno);
+	$email = mysq_real_escape_string($email);
+	$memberno = mysql_real_escape_string($memberno);
+		
+	$sql = "SELECT * FROM $tbl WHERE email = '$email' and member_number = '$memberno'";
+	$result = mysql_query($sql);
+	
+	//checks to make sure the result returned is within one row in the database
+	$row = mysql_num_rows($result);
+	
+	if($row == 1)
+	{
+		session_register("email");
+		session_register("memberno");
+		header("location:memberwelcome.php");
+	}
+	else
+	{
+		echo "You have entered an incorrect username or password.";	
+	}
+		
+	?>
+	</div>
+	<div id="footer">
+	<?php
+	echo date('jS F');
+	?>
+	</div>
+</div>
+</form>
+</body>
+</html>
+	
