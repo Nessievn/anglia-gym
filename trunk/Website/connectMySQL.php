1235e8f8ce86fc6d309474d7f45ec659707f0190
sam.pratley <sam.pratley@f61b0cec-0cda-11df-a5af-b38b39d06e06>
2010-04-04 10:22:25 +0000

    contains connection info for connecting to the database

---
+++ trunk/Website/connectMySQL.php
@@ -0,0 +1,10 @@
+function connect()
+{
+	$host="localhost";
+	$username="gym";
+	$password="gym";
+	$dbname="gym";
+
+	$link = mysql_connect($host,$username,$password) or die("cannot connect");
+	mysql_select_db($dbname,$link) or die ("cannot select database");
+}
